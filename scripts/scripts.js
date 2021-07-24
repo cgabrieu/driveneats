cd97e2bc62b74661e791bf4e84e1aff765f766cc
Carlos Gabriel <carlosgabrielpc@live.com>
2021-07-24 20:00:44 -0300

    Condição para atualizar classe botão Concluir Pedido

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,6 +6,8 @@ function selecionar(card, secao) {
     card.classList.add("selecionado");
 
     if(document.querySelectorAll(".selecionado").length === 3){
-        alert("Selecionou 3");
+        const botaoConcluir = document.querySelector(".botao-concluir");
+        botaoConcluir.innerHTML = "Fechar pedido";
+        botaoConcluir.classList.add("fechar-pedido-botao");
     }
 }
