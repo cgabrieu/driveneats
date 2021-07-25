619c26b4b82b74baf5eefab048ce6fc81733d4ec
Carlos Gabriel <carlosgabrielpc@live.com>
2021-07-25 18:21:02 -0300

    popup funcionando e melhorias no javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,26 +21,32 @@ function converterValor(valor){
     return Number(valor.replace(",","."));
 }
 
-function abrirPopup(){
-    document.querySelector(".fundo-confirme-pedido").classList.remove("ocultar");
+function popup(a){
+    if (a === false) document.querySelector(".fundo-confirme-pedido").classList.remove("flex-center");
+    else {
+        document.querySelector(".fundo-confirme-pedido").classList.add("flex-center")
+        
+    }
+}
 
-    const prato = document.querySelector(".prato .selecionado h4").innerHTML;
-    const bebida = document.querySelector(".bebida .selecionado h4").innerHTML;
-    const sobremesa = document.querySelector(".sobremesa .selecionado h4").innerHTML;
-    
-    const valor = (converterValor(document.querySelector(".prato .selecionado strong").innerHTML)
-    + converterValor(document.querySelector(".bebida .selecionado strong").innerHTML)
-    + converterValor(document.querySelector(".sobremesa .selecionado strong").innerHTML)).toFixed(2);
-
+function finalizarPedido(){
     const msg = encodeURIComponent(`Olá, gostaria de fazer o pedido:
     - *Prato*: ${prato}
     - *Bebida*: ${bebida}
     - *Sobremesa*: ${sobremesa}
-    *Total: R$ ${valor}*`);
-    const numFone = "5527999439806";  
-}
-
-function finalizarPedido(numFone, msg) {
+    *Total: R$ ${valor}*
+    `);
+    const numFone = "5527999439806";
     window.open(`https://wa.me/${numFone}?text=${msg}`);
 }
 
+function obterItem(e){
+    if (e == "prato") return document.querySelector(".prato .selecionado h4").innerHTML;
+    if (e == "bebida") return document.querySelector(".bebida .selecionado h4").innerHTML;
+    if (e == "sobremesa") return document.querySelector(".sobremesa .selecionado h4").innerHTML;
+    if (e == "valorPrato") return converterValor(document.querySelector(".prato .selecionado strong").innerHTML);
+    if (e == "valorBebida") return converterValor(document.querySelector(".bebida .selecionado strong").innerHTML);
+    if (e == "valorSobremesa") return converterValor(document.querySelector(".sobremesa .selecionado strong").innerHTML)
+    if (e == "total") return (valorPrato+valorBebida+valorSobremesa).toFixed(2);
+    return("Error");
+}
