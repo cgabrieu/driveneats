2820229952398562cf8aa9f650792669f8b58452
Carlos Gabriel <carlosgabrielpc@live.com>
2021-07-24 21:12:22 -0300

    Versão obrigatória pronta (acho)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+let botaoFecharPedido = false;
+
 function selecionar(card, secao) {
     const selecionado = document.querySelector(`.${secao} .selecionado`);
 
@@ -9,5 +11,29 @@ function selecionar(card, secao) {
         const botaoConcluir = document.querySelector(".botao-concluir");
         botaoConcluir.innerHTML = "Fechar pedido";
         botaoConcluir.classList.add("fechar-pedido-botao");
+        botaoFecharPedido = true;
     }
+}
+
+function converterValor(valor){
+    return Number(valor.replace(",","."));
+}
+
+function finalizarPedido(){
+    const prato = document.querySelector(".prato .selecionado h4").innerHTML;
+    const bebida = document.querySelector(".bebida .selecionado h4").innerHTML;
+    const sobremesa = document.querySelector(".sobremesa .selecionado h4").innerHTML;
+    
+    const valor = (converterValor(document.querySelector(".prato .selecionado strong").innerHTML)
+    + converterValor(document.querySelector(".bebida .selecionado strong").innerHTML)
+    + converterValor(document.querySelector(".sobremesa .selecionado strong").innerHTML)).toFixed(2);
+
+    const msg = encodeURIComponent(`Olá, gostaria de fazer o pedido:
+    - Prato: ${prato}
+    - Bebida: ${bebida}
+    - Sobremesa: ${sobremesa}
+    Total: R$ ${valor}
+    `);
+
+    window.open(`https://wa.me/5527999439806?text=${msg}`);
 }
