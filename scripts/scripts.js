8000a980e3b0f44bfc19961e9df43d1835b8a308
Carlos Gabriel <carlosgabrielpc@live.com>
2021-07-25 19:25:37 -0300

    finalizado com bônus

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,37 +16,49 @@ function selecionar(card, secao) {
 }
 
 function converterValor(valor){
-    if(typeof(valor) === "number")
-        return valor.toString().replace(".",",");
     return Number(valor.replace(",","."));
 }
 
 function popup(a){
     if (a === false) document.querySelector(".fundo-confirme-pedido").classList.remove("flex-center");
     else {
-        document.querySelector(".fundo-confirme-pedido").classList.add("flex-center")
-        
+        document.querySelector(".fundo-confirme-pedido").classList.add("flex-center");
+        document.querySelector(".nome-prato").innerHTML = obterItem("prato");
+        document.querySelector(".valor-prato").innerHTML = obterItem("valorPrato");
+        document.querySelector(".nome-bebida").innerHTML = obterItem("bebida");
+        document.querySelector(".valor-bebida").innerHTML = obterItem("valorBebida");
+        document.querySelector(".nome-sobremesa").innerHTML = obterItem("sobremesa");
+        document.querySelector(".valor-sobremesa").innerHTML = obterItem("valorSobremesa");
+        document.querySelector(".total-itens strong").innerHTML = obterItem("total");
     }
 }
 
 function finalizarPedido(){
     const msg = encodeURIComponent(`Olá, gostaria de fazer o pedido:
-    - *Prato*: ${prato}
-    - *Bebida*: ${bebida}
-    - *Sobremesa*: ${sobremesa}
-    *Total: R$ ${valor}*
+    - *Prato*: ${obterItem("prato")}
+    - *Bebida*: ${obterItem("bebida")}
+    - *Sobremesa*: ${obterItem("sobremesa")}
+    *Total: R$ ${obterItem("total")}*
+
+    *Nome*: ${prompt("Infome seu nome:")}
+    *Endereço*: ${prompt("Infome seu endereço:")}
     `);
     const numFone = "5527999439806";
     window.open(`https://wa.me/${numFone}?text=${msg}`);
 }
 
 function obterItem(e){
+    const valorPrato = document.querySelector(".prato h6 strong").innerHTML;
+    const valorBebida = document.querySelector(".bebida h6 strong").innerHTML;
+    const valorSobremesa = document.querySelector(".sobremesa h6 strong").innerHTML;
+
     if (e == "prato") return document.querySelector(".prato .selecionado h4").innerHTML;
-    if (e == "bebida") return document.querySelector(".bebida .selecionado h4").innerHTML;
-    if (e == "sobremesa") return document.querySelector(".sobremesa .selecionado h4").innerHTML;
-    if (e == "valorPrato") return converterValor(document.querySelector(".prato .selecionado strong").innerHTML);
-    if (e == "valorBebida") return converterValor(document.querySelector(".bebida .selecionado strong").innerHTML);
-    if (e == "valorSobremesa") return converterValor(document.querySelector(".sobremesa .selecionado strong").innerHTML)
-    if (e == "total") return (valorPrato+valorBebida+valorSobremesa).toFixed(2);
+    else if (e == "bebida") return document.querySelector(".bebida .selecionado h4").innerHTML;
+    else if (e == "sobremesa") return document.querySelector(".sobremesa .selecionado h4").innerHTML;   
+    else if (e == "valorPrato") return valorPrato; 
+    else if (e == "valorBebida") return valorBebida; 
+    else if (e == "valorSobremesa") return valorSobremesa; 
+    else if (e == "total")
+        return (converterValor(valorPrato)+converterValor(valorBebida)+converterValor(valorSobremesa)).toFixed(2).replace(".",",");
     return("Error");
 }
